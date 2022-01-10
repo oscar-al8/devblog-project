a1c0366d7a8f4a4e1608b71ddeff6fa7bec48265
oscar_al8 <oscarmehrer@hotmail.com>
2022-01-10 15:52:33 -0400

    issue fixed

--- scripts/cache.js
+++ scripts/cache.js
@@ -1,6 +1,6 @@
-const fs =  require('fs')
-const path =  require('path')
-const matter =  require('gray-matter')
+const fs = require('fs')
+const path = require('path')
+const matter = require('gray-matter')
 
 function postData(){
 
