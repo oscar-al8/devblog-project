4b5149cbc2e86067acde7d12ab2271067c92eb55
oscar_al8 <oscarmehrer@hotmail.com>
2022-01-10 13:59:37 -0400

    preparing for deployment

--- devblog/components/Post.js
+++ devblog/components/Post.js
@@ -2,10 +2,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 import CategoryLabel from './CategoryLabel'
 
-export default function Post({post}) {
+export default function Post({post, compact}) {
     return (
         <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
-            <Image src={post.frontmatter.cover_image} alt='' height={420} width={600} className='mb-4 rounded'></Image>
+            {!compact && (
+                <Image src={post.frontmatter.cover_image} alt='' height={420} width={600} className='mb-4 rounded'></Image>
+            )}
             <div className="flex justify-between items-center">
                 <span className="font-light text-gray-600">
                     {post.frontmatter.date}
@@ -22,17 +24,19 @@ export default function Post({post}) {
                     {post.frontmatter.excerpt}
                 </p>
             </div>
-            <div className="flex justify-between items-center mt-6">
-                <Link href={`/blog/${post.slug}`}>
-                    <a className="text-gray-900 hover:text-blue-600">
-                        Read More
-                    </a>
-                </Link>
-                <div className="flex items-center">
-                    <img src={post.frontmatter.author_image} alt="" className='mx-4 w-10 object-cover rounded-full hidden sm:block' />
-                    <h3 className="text-gray-700 font-bold">{post.frontmatter.author}</h3>
+            {!compact && (
+                <div className="flex justify-between items-center mt-6">
+                    <Link href={`/blog/${post.slug}`}>
+                        <a className="text-gray-900 hover:text-blue-600">
+                            Read More
+                        </a>
+                    </Link>
+                    <div className="flex items-center">
+                        <img src={post.frontmatter.author_image} alt="" className='mx-4 w-10 object-cover rounded-full hidden sm:block' />
+                        <h3 className="text-gray-700 font-bold">{post.frontmatter.author}</h3>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
--- devblog/pages/blog/page/[page_index].js
+++ devblog/pages/blog/page/[page_index].js
@@ -5,20 +5,26 @@ import Post from "@/components/Post"
 import Pagination from "@/components/Pagination"
 import {POSTS_PER_PAGE} from '@/config/index'
 import { getPosts } from '@/lib/posts'
+import CategoryList from '@/components/CategoryList'
 
-export default function BlogPage({posts, numPages, currentPage}) {
-
-
+export default function BlogPage({posts, numPages, currentPage, categories}) {
 
     return (
         <Layout>
-        <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {posts.map((post, index) => (
-            <Post key={index} post={post}></Post>
-            ))}
-        </div>
-        <Pagination currentPage={currentPage} numPages={numPages} />
+            <div className="flex justify-between">
+                <div className="w-3/4 mr-10">
+                    <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+                        {posts.map((post, index) => (
+                        <Post key={index} post={post}></Post>
+                        ))}
+                    </div>
+                    <Pagination currentPage={currentPage} numPages={numPages} />
+                </div>
+                <div className="w-1/4">
+                    <CategoryList categories={categories} />
+                </div>
+            </div>
         </Layout>
     )
 }
@@ -44,18 +50,23 @@ export async function getStaticProps({params}){
 
     const page = parseInt((params && params.page_index || 1))
 
-    const files =  fs.readdirSync(path.join('posts'));
+    const files =  fs.readdirSync(path.join('posts'))
     const posts = getPosts()
 
-  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
-  const pageIndex = page - 1
-  const orderedPosts = posts.slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
+    // Get categories for sidebar
+    const categories = posts.map(post => post.frontmatter.category)
+    const uniqueCategories = [...new Set(categories)]
 
-  return{
-    props: {
-      posts: orderedPosts,
-      numPages,
-      currentPage: page
-    },
-  }
+    const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+    const pageIndex = page - 1
+    const orderedPosts = posts.slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
+
+    return{
+        props: {
+        posts: orderedPosts,
+        numPages,
+        currentPage: page,
+        categories: uniqueCategories
+        },
+    }
 }
